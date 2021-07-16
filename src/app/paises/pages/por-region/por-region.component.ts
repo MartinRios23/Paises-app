513c0a14685bce68fc276cdf24aa284a37a2c1aa
MartinRios23 <martinrioscjs@gmail.com>
2021-07-16 18:06:10 -0300

    Updating proyect

--- src/app/paises/pages/por-region/por-region.component.ts
+++ src/app/paises/pages/por-region/por-region.component.ts
@@ -3,14 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [`button{margin-right: 5px}`]
 })
 export class PorRegionComponent implements OnInit {
+  regiones: string[] = [ 'africa', 'americas', 'asia', 'europe', 'oceania'];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
