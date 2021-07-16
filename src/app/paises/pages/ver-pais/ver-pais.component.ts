36cec08fd7aaeb7aae7eca6122dc0eda5482c015
MartinRios23 <martinrioscjs@gmail.com>
2021-07-16 17:05:51 -0300

    Updating proyect

--- src/app/paises/pages/ver-pais/ver-pais.component.ts
+++ src/app/paises/pages/ver-pais/ver-pais.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PaisesService } from '../../services/paises.service';
+import { Country } from '../../interfaces/paises.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor(private activatedRoute:ActivatedRoute) { }
+  pais!:Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisesService
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe();
-  }
+    this.activatedRoute.params.subscribe((params) => {
+      console.log(params.id);
 
+      this.paisService.getPaisporCodigo(params.id).subscribe((pais) => {
+        console.log(pais);
+        this.pais = pais;
+      });
+    });
+  }
 }
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -4,17 +4,17 @@ import { Observable } from 'rxjs';
 import { Country } from '../interfaces/paises.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaisesService {
-
   private api_url = 'https://restcountries.eu/rest/v2';
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
+
   /**
    * @param termino
    * @returns Array of Countrys (For country name)
    */
-  buscarPais(termino:string):Observable<Country[]>{
+  buscarPais(termino: string): Observable<Country[]> {
     const url = `${this.api_url}/name/${termino}`;
     return this.http.get<Country[]>(url);
   }
@@ -24,8 +24,18 @@ export class PaisesService {
    * @param termino
    * @returns Array of countrys (For capital)
    */
-  buscarCapital(termino:string):Observable<Country[]>{
-    const url_capital = `${this.api_url}/capital/${termino}`
+  buscarCapital(termino: string): Observable<Country[]> {
+    const url_capital = `${this.api_url}/capital/${termino}`;
     return this.http.get<Country[]>(url_capital);
+  }
+
+  /**
+   *
+   * @param id
+   * @returns Country for ID
+   */
+  getPaisporCodigo(id: string): Observable<Country> {
+    const url = `${this.api_url}/alpha/${id}`;
+    return this.http.get<Country>(url);
   }
 }
